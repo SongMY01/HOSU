7bc8bf499c8ce1f70af9387cbe443a486ea35043
SongMY01 <1018myone@gmail.com>
2026-08-23 11:29:10 +0900

    feat(ui): redesign entire frontend to crisp White / Light mode with Carto Positron map tiles

--- 2_regional_service/frontend/src/components/Map/MapView.jsx
+++ 2_regional_service/frontend/src/components/Map/MapView.jsx
@@ -28,7 +28,7 @@ export default function MapView({ regions, shelters, showShelters, onSelectRegio
       >
         <ZoomControl position="topright" />
         <TileLayer
-          url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+          url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
           attribution="&copy; OpenStreetMap &copy; CARTO"
           subdomains="abcd"
           maxZoom={18}
